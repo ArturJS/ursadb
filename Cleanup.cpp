7273824123b8279eb649aed1b976f14c5b20da89
Michał Leszczyński <michal.leszczynski@cert.pl>
2019-11-27 19:09:23 +0100

    better

--- Cleanup.cpp
+++ Cleanup.cpp
@@ -39,6 +39,7 @@ int main(int argc, char *argv[]) {
     Database db(db_name);
 
     std::set<std::string> db_files;
+    std::vector<fs::path> remove_list;
     db_files.insert(db.get_name());
 
     for (OnDiskDataset *workingSet : db.working_sets()) {
@@ -53,35 +54,54 @@ int main(int argc, char *argv[]) {
     }
 
     fs::directory_iterator end;
+    unsigned int legit_files = 0;
 
     for (fs::directory_iterator dir(db.get_base()); dir != end; ++dir) {
         if (fs::is_regular_file(dir->path())) {
             fs::path fn = dir->path().filename();
 
-            if (endsWith(fn.string(), "." + db.get_name().string())) {
+            if (fn.string() == db.get_name()) {
+                std::cout << "required file " << fn.string() << std::endl;
+                legit_files++;
+            } else if (endsWith(fn.string(), "." + db.get_name().string())) {
                 if (db_files.find(fn.string()) == db_files.end()) {
-                    std::cout << "unlinked file " << fn.string();
-
-                    if (!dry_run) {
-                        if (fs::remove(dir->path())) {
-                            std::cout << " deleted succesfully";
-                        } else {
-                            std::cout << " failed to delete";
-                        }
-                    }
-
-                    std::cout << std::endl;
+                    std::cout << "unlinked file " << fn.string() << std::endl;
+                    remove_list.push_back(dir->path());
                 } else {
                     std::cout << "required file " << fn.string() << std::endl;
+                    legit_files++;
                 }
             }
         }
+    }
+
+    if (legit_files != db_files.size()) {
+        std::cout << "something went wrong, found only " << legit_files << " out of " << db_files.size() << " declared database files" << std::endl;
+        return 1;
     }
 
     if (dry_run) {
         std::cout << "in order to really remove unlinked files, execute:" << std::endl;
         std::cout << "    ursadb_cleanup --confirm " + db_name << std::endl;
         std::cout << "be warned that executing this command is not safe it UrsaDB is running" << std::endl;
+    }
+
+    if (!remove_list.size()) {
+        std::cout << "everything OK, database seems to be in consistent state" << std::endl;
+    } else {
+        std::cout << "found " << remove_list.size() << " dangling files" << std::endl;
+    }
+
+    if (!dry_run) {
+        for (fs::path &fn : remove_list) {
+            std::cout << "delete " << fs::absolute(fn).string();
+
+            if (fs::remove(fn)) {
+                std::cout << " OK" << std::endl;
+            } else {
+                std::cout << " FAILED" << std::endl;
+            }
+        }
     }
 
     return 0;
